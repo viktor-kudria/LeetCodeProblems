5eeb77ece3ae366aceb97d7d09bf1abf8ebad595
Viktor Kudria <vkudria@csod.com>
2019-11-29 17:20:13 +0200

    Resolve Two Sum problem

---
+++ LeetCodeProblems.Problems/Array/1_TwoSumProblem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeProblems.Problems.Array
+{
+    /// <summary>
+    /// Two Sum problem
+    /// https://leetcode.com/problems/two-sum/
+    /// </summary>
+    public class TwoSumProblem
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var result = new int[] {0, 0};
+            var dict = new Dictionary<int, int>();
+            int dif;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dif = target - nums[i];
+
+                if (dict.ContainsKey(dif))
+                {
+                    result[0] = dict[dif];
+                    result[1] = i;
+                }
+                else
+                {
+                    dict.TryAdd(nums[i], i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeProblems.Tests/Array/1_TwoSumTests.cs
@@ -0,0 +1,63 @@
+﻿using LeetCodeProblems.Problems.Array;
+using Xunit;
+
+namespace LeetCodeProblems.Tests.Array
+{
+    public class TwoSumTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var input = new Input()
+            {
+                Nums = new[] {2, 7, 11, 15},
+                Target = 9
+            };
+            var expected = new [] {0, 1};
+            var problem = new TwoSumProblem();
+
+            var actual = problem.TwoSum(input.Nums, input.Target);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test2_Negative()
+        {
+            var input = new Input()
+            {
+                Nums = new[] { -2, 7, 11, 15 },
+                Target = 9
+            };
+            var expected = new[] { 0, 2 };
+            var problem = new TwoSumProblem();
+
+            var actual = problem.TwoSum(input.Nums, input.Target);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test3_Duplicates()
+        {
+            var input = new Input()
+            {
+                Nums = new[] { -2, 7, 11, 15, 15, 4 },
+                Target = 19
+            };
+            var expected = new[] { 3, 5 };
+            var problem = new TwoSumProblem();
+
+            var actual = problem.TwoSum(input.Nums, input.Target);
+
+            Assert.Equal(expected, actual);
+        }
+
+        private class Input
+        {
+            public int[] Nums { get; set; }
+
+            public int Target { get; set; }
+        }
+    }
+}
