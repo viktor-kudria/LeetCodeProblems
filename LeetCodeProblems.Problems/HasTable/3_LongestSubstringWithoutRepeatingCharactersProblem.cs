efb0ce9f1bfeeb8528338c7ac2a5147ca0d8c69d
Viktor Kudria <vkudria@csod.com>
2019-11-30 20:16:12 +0200

    3. Longest Substring Without Repeating Characters | Resolve

---
+++ LeetCodeProblems.Problems/HasTable/3_LongestSubstringWithoutRepeatingCharactersProblem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeProblems.Problems.HasTable
+{
+    public class LongestSubstringWithoutRepeatingCharactersProblem
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int length = 0;
+            int currentLength = 0;
+            var dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.ContainsKey(s[i]))
+                {
+                    if (dict[s[i]] > i - currentLength - 1)
+                    {
+                        if (currentLength > length)
+                        {
+                            length = currentLength;
+                        }
+
+                        currentLength = i - dict[s[i]];
+
+                        if (currentLength + s.Length - i <= length)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        currentLength++;
+                    }
+
+                    dict[s[i]] = i;
+                }
+                else
+                {
+                    dict.Add(s[i], i);
+                    currentLength++;
+                }
+            }
+
+            if (currentLength > length)
+            {
+                length = currentLength;
+            }
+
+            return length;
+        }
+    }
+}
---
+++ LeetCodeProblems.Tests/HashTable/3_LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,63 @@
+﻿using LeetCodeProblems.Problems.HasTable;
+using Xunit;
+
+namespace LeetCodeProblems.Tests.HashTable
+{
+    public class LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var problem = new LongestSubstringWithoutRepeatingCharactersProblem();
+            var input = "abcabcbb";
+            var actual = problem.LengthOfLongestSubstring(input);
+            int expected = 3;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var problem = new LongestSubstringWithoutRepeatingCharactersProblem();
+            var input = "aaaaaa";
+            var actual = problem.LengthOfLongestSubstring(input);
+            int expected = 1;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var problem = new LongestSubstringWithoutRepeatingCharactersProblem();
+            var input = "pwwkew";
+            var actual = problem.LengthOfLongestSubstring(input);
+            int expected = 3;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var problem = new LongestSubstringWithoutRepeatingCharactersProblem();
+            var input = "abba";
+            var actual = problem.LengthOfLongestSubstring(input);
+            int expected = 2;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            var problem = new LongestSubstringWithoutRepeatingCharactersProblem();
+            var input = "tmmzuxt";
+            var actual = problem.LengthOfLongestSubstring(input);
+            int expected = 5;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
