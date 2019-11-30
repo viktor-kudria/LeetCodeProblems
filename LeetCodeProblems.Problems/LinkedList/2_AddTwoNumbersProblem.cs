0f1194ae3d0c44ce55c43ae46ae9cf1c68c96106
Viktor Kudria <vkudria@csod.com>
2019-11-30 19:28:21 +0200

    2. Add Two Numbers | Resolve

---
+++ LeetCodeProblems.Problems/LinkedList/2_AddTwoNumbersProblem.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCodeProblems.Problems.LinkedList
+{
+    /// <summary>
+    /// 2. Add Two Numbers
+    /// https://leetcode.com/problems/add-two-numbers/
+    /// </summary>
+    public class AddTwoNumbersProblem
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            int sum;
+            int tmp = 0;
+            ListNode resultRoot = new ListNode(0);
+            ListNode currListNode = resultRoot;
+
+            do
+            {
+                sum = l1.val + l2.val + tmp;
+
+                if (sum > 9)
+                {
+                    tmp = 1;
+                    currListNode.val = sum - 10;
+                }
+                else
+                {
+                    tmp = 0;
+                    currListNode.val = sum;
+                }
+
+                if (l1.next == null && l2.next == null && tmp == 0)
+                {
+                    break;
+                }
+
+                if (l1.next != null)
+                {
+                    l1 = l1.next;
+                }
+                else
+                {
+                    l1.val = 0;
+                }
+
+                if (l2.next != null)
+                {
+                    l2 = l2.next;
+                }
+                else
+                {
+                    l2.val = 0;
+                }
+
+                currListNode.next = new ListNode(0);
+                currListNode = currListNode.next;
+            } while (true);
+
+            return resultRoot;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
---
+++ LeetCodeProblems.Tests/LinkedList/2_AddTwoNumbersTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using LeetCodeProblems.Problems.LinkedList;
+using Xunit;
+
+namespace LeetCodeProblems.Tests.LinkedList
+{
+    public class AddTwoNumbersTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var problem = new AddTwoNumbersProblem();
+
+            var input1 = CreateList(new[] { 2, 4, 3 });
+            var input2 = CreateList(new[] { 5, 6, 4 });
+
+            var expected = CreateList(new[] { 7, 0, 8 });
+
+            var actual = problem.AddTwoNumbers(input1, input2);
+
+            CheckResult(expected, actual);
+        }
+
+        [Fact]
+        public void Test2_DifferentLength()
+        {
+            var problem = new AddTwoNumbersProblem();
+
+            var input1 = CreateList(new[] { 2, 4, 3, 1 });
+            var input2 = CreateList(new[] { 5, 6, 4 });
+
+            var expected = CreateList(new[] { 7, 0, 8, 1 });
+
+            var actual = problem.AddTwoNumbers(input1, input2);
+
+            CheckResult(expected, actual);
+        }
+
+        [Fact]
+        public void Test3_DifferentLength()
+        {
+            var problem = new AddTwoNumbersProblem();
+
+            var input1 = CreateList(new[] { 2, 4, 3 });
+            var input2 = CreateList(new[] { 5, 6, 4, 1 });
+
+            var expected = CreateList(new[] { 7, 0, 8, 1 });
+
+            var actual = problem.AddTwoNumbers(input1, input2);
+
+            CheckResult(expected, actual);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var problem = new AddTwoNumbersProblem();
+
+            var input1 = CreateList(new[] { 9 });
+            var input2 = CreateList(new[] { 9 });
+
+            var expected = CreateList(new[] { 8, 1 });
+
+            var actual = problem.AddTwoNumbers(input1, input2);
+
+            CheckResult(expected, actual);
+        }
+
+        private ListNode CreateList(int[] arr)
+        {
+            var rootNode = new ListNode(arr[0]);
+            var currentNode = rootNode;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                currentNode.next = new ListNode(arr[i]);
+                currentNode = currentNode.next;
+            }
+
+            return rootNode;
+        }
+
+        private void CheckResult(ListNode expected, ListNode actual)
+        {
+            do
+            {
+                Assert.Equal(expected.val, actual.val);
+                if (expected.next != null || actual.next != null)
+                {
+                    expected = expected.next;
+                    actual = actual.next;
+
+                    continue;
+                }
+
+                return;
+            } while (true);
+        }
+    }
+}
