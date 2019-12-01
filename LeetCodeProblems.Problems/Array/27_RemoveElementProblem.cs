835e9fa34c2f06868e372ba8549ee0bf06950e4c
Viktor Kudria <v.a.kudrya@gmail.com>
2019-12-01 18:46:43 +0200

    27. Remove Element | Resolve

---
+++ LeetCodeProblems.Problems/Array/27_RemoveElementProblem.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeProblems.Problems.Array
+{
+    /// <summary>
+    /// 27. Remove Element
+    /// https://leetcode.com/problems/remove-element/
+    /// </summary>
+    public class RemoveElementProblem
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums.Length == 0) return 0;
+
+            int indexToPut = nums.Length - 1;
+            int length = 0;
+            for (int i = 0; i < nums.Length && i <= indexToPut; i++)
+            {
+                if (nums[i] == val)
+                {
+                    while (nums[indexToPut] == val)
+                    {
+                        if (indexToPut == i) return length;
+
+                        indexToPut--;
+                    }
+
+                    nums[indexToPut] = nums[indexToPut] + nums[i];
+                    nums[i] = nums[indexToPut] - nums[i];
+                    nums[indexToPut] = nums[indexToPut] - nums[i];
+
+                    length++;
+                }
+                else
+                {
+                    length++;
+                }
+            }
+
+            return length;
+        }
+    }
+}
---
+++ LeetCodeProblems.Tests/Array/27_RemoveElementTests.cs
@@ -0,0 +1,87 @@
+﻿using LeetCodeProblems.Problems.Array;
+using Xunit;
+
+namespace LeetCodeProblems.Tests.Array
+{
+    public class RemoveElementTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var problem = new RemoveElementProblem();
+
+            var input = new Input()
+            {
+                Nums = new[] {3, 2, 2, 3},
+                Val = 3
+            };
+
+            var expected = new ExpectedOutput()
+            {
+                Nums = new[] {2, 2},
+                Length = 2
+            };
+
+            var actual = problem.RemoveElement(input.Nums, input.Val);
+
+            Assert.Equal(expected.Length, actual);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var problem = new RemoveElementProblem();
+
+            var input = new Input()
+            {
+                Nums = new[] { 2 },
+                Val = 3
+            };
+
+            var expected = new ExpectedOutput()
+            {
+                Nums = new[] { 2 },
+                Length = 1
+            };
+
+            var actual = problem.RemoveElement(input.Nums, input.Val);
+
+            Assert.Equal(expected.Length, actual);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var problem = new RemoveElementProblem();
+
+            var input = new Input()
+            {
+                Nums = new[] { 2, 2 },
+                Val = 2
+            };
+
+            var expected = new ExpectedOutput()
+            {
+                Length = 0
+            };
+
+            var actual = problem.RemoveElement(input.Nums, input.Val);
+
+            Assert.Equal(expected.Length, actual);
+        }
+
+        private class Input
+        {
+            public int[] Nums { get; set; }
+
+            public int Val { get; set; }
+        }
+
+        private class ExpectedOutput
+        {
+            public int[] Nums { get; set; }
+
+            public int Length { get; set; }
+        }
+    }
+}
