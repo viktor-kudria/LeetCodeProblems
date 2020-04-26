0e0bc1c2c78dbdffdf6cb89b7e25cb746e566002
Viktor Kudria <v.a.kudrya@gmail.com>
2020-04-26 18:54:15 +0300

    53. Maximum subarray | Add Divide And Conquer version

--- LeetCodeProblems.Problems/Array/53_MaximumSubarrayProblem.cs
+++ LeetCodeProblems.Problems/Array/53_MaximumSubarrayProblem.cs
@@ -1,10 +1,12 @@
-﻿namespace LeetCodeProblems.Problems.Array
+﻿using System;
+
+namespace LeetCodeProblems.Problems.Array
 {
     public class MaximumSubarrayProblem
     {
         public int MaxSubArray(int[] nums)
         {
-            return Kadane(nums);
+            return DivideAndConquer(nums);
         }
 
         private int Kadane(int[] nums)
@@ -19,6 +21,42 @@
             }
 
             return bestSum;
+        }
+
+        private int DivideAndConquer(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            return MaxSubArray(nums, 0, nums.Length - 1);
+
+            int MaxSubArray(int[] arr, int left, int right)
+            {
+                if (left == right) return arr[left];
+
+                var middle = (left + right) / 2;
+
+                var bestLeft = MaxSubArray(arr, left, middle);
+                var bestRight = MaxSubArray(arr, middle + 1, right);
+
+                int leftMax = arr[middle];
+                int rightMax = arr[middle + 1];
+
+                int tmp = 0;
+                for (int i = middle; i >= left; i--)
+                {
+                    tmp += arr[i];
+                    if (tmp > leftMax) leftMax = tmp;
+                }
+
+                tmp = 0;
+                for (int i = middle + 1; i <= right; i++)
+                {
+                    tmp += arr[i];
+                    if (tmp > rightMax) rightMax = tmp;
+                }
+
+                return Math.Max(Math.Max(bestLeft, bestRight), leftMax + rightMax);
+            }
         }
     }
 }
