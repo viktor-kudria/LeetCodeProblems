1c58893308ad9333bfee07dee1469393f161c3db
Viktor Kudria <v.a.kudrya@gmail.com>
2020-04-26 18:20:48 +0300

    53. Maximum subarray | Kadane's algorithm

--- LeetCodeProblems.Problems/Array/53_MaximumSubarrayProblem.cs
+++ LeetCodeProblems.Problems/Array/53_MaximumSubarrayProblem.cs
@@ -4,69 +4,21 @@
     {
         public int MaxSubArray(int[] nums)
         {
-            return Calculate2(nums, 0, nums.Length - 1);
+            return Kadane(nums);
         }
 
-        private int Calculate(int[] nums, int startIndex, int endIndex)
+        private int Kadane(int[] nums)
         {
-            int sum = 0;
-            if (startIndex == endIndex) return nums[startIndex];
+            int bestSum = int.MinValue;
+            int currentSum = 0;
 
-            for (int i = startIndex; i <= endIndex; i++)
+            foreach (var num in nums)
             {
-                sum += nums[i];
+                currentSum = currentSum + num > num ? currentSum + num : num;
+                bestSum = currentSum > bestSum ? currentSum : bestSum;
             }
 
-            int leftSum = Calculate(nums, startIndex, endIndex - 1);
-            int rightSum = Calculate(nums, startIndex + 1, endIndex);
-
-            if (sum > leftSum)
-            {
-                return sum > rightSum ? sum : rightSum;
-            }
-
-            return leftSum > rightSum ? leftSum : rightSum;
-        }
-
-        private int Calculate2(int[] nums, int startIndex, int endIndex)
-        {
-            int sum = 0;
-            int deepSum = 0;
-            if (startIndex == endIndex) return nums[startIndex];
-
-            for (int i = startIndex; i <= endIndex; i++)
-            {
-                sum += nums[i];
-            }
-
-            int leftSum = sum - nums[endIndex];
-            int rightSum = sum - nums[startIndex];
-
-            if (leftSum > rightSum)
-            {
-                int calc = Calculate2(nums, startIndex, endIndex - 1);
-                deepSum = leftSum > calc ? leftSum : calc;
-            }
-            else if (rightSum > leftSum)
-            {
-                int calc = Calculate2(nums, startIndex + 1, endIndex);
-                deepSum = rightSum > calc ? rightSum : calc;
-            }
-            else
-            {
-                int calcLeft = Calculate2(nums, startIndex, endIndex - 1);
-                int calcRight = Calculate2(nums, startIndex + 1, endIndex);
-
-                int calc = calcLeft > calcRight ? calcLeft : calcRight;
-                deepSum = rightSum > calc ? rightSum : calc;
-            }
-
-            return sum > deepSum ? sum : deepSum;
-        }
-
-        private int Calculate3(int[] nums, int startIndex, int endIndex)
-        {
-            
+            return bestSum;
         }
     }
 }
--- LeetCodeProblems.Tests/Array/53_MaximumSubarrayTests.cs
+++ LeetCodeProblems.Tests/Array/53_MaximumSubarrayTests.cs
@@ -53,7 +53,7 @@ namespace LeetCodeProblems.Tests.Array
                 -25, -96, -83, -46, -15, -68, -35, -65, -44, -51, -88, -9, -77, -79, -89, -85,
                 -4, -52, -55, -100, -33, -61, -77, -69, -40, -13, -27, -87, -95, -40 };
 
-            var expected = 10;
+            var expected = -3;
 
             var actual = problem.MaxSubArray(input);
 
